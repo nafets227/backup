9b7624308c0f8d9b9fa7105c3482e7154e97fcee
Stefan Schallenberg <nafets227@users.noreply.github.com>
2020-06-09 09:27:41 +0200

    Beautify rsync.sh (indention)

--- backup.d/rsync.sh
+++ backup.d/rsync.sh
@@ -3,20 +3,20 @@
 # backup_rsync
 # (C) 2015 Stefan Schallenberg
 #
-# Usage: backup_rsync [ --hist | --inet | --inetlog=<logfile> ] <source> <dest> 
+# Usage: backup_rsync [ --hist | --inet | --inetlog=<logfile> ] <source> <dest>
 #            Copies all files from <source> to <dest> recursively, using rsync
 #
 #    Add Ons to rsync:
 #        --hist enable HISTORY mode (see below)
 #        --inet enable INTERNE mode (see below)
-#        --inetlog=<logfile> enable INTERNET mode (see below) 
+#        --inetlog=<logfile> enable INTERNET mode (see below)
 #                            and write LOG (see below)
 #        --ssh enable SSH mode (connect to remote rsync via SSH)
 #        --sshlog=<logfile> enable SSH mode and write to log
 #        <source> may be of format machine:/directory, then SSH is used.
-#        <dest> does NOT support remote machines     
-#            
-#    HISTORY MODE: 
+#        <dest> does NOT support remote machines
+#
+#    HISTORY MODE:
 #        History is supposed to create daily backups with hard-link to previous
 #        backups for unmodified files.
 #            1) a Directory named like 20151107 is created in DEST directoy
@@ -26,7 +26,7 @@
 #            4) modified files will be copied
 #            5) dest directoy may be local only
 #
-#    INTERNET MODE: 
+#    INTERNET MODE:
 #         Internet Mode is designed to support target on WebDav system.
 #            1) Timestamps are not reliable (most WebDav server always put
 #               the current timestamp at write time and do now allow to
@@ -44,237 +44,236 @@
 #
 
 function backup_rsync {
-local readonly debug=${debug:-0}
-local hist=0
-local inetlog=0
-local logfile=""
-local prm=""
-local histparm=""
-local currback=""
-local lastback=""
-local opt="-aHX --delete"
-local optrsh="--rsh=ssh"
-local allowSrcRemote=1
-local allowDstRemote=0
+	local readonly debug=${debug:-0}
+	local hist=0
+	local inetlog=0
+	local logfile=""
+	local prm=""
+	local histparm=""
+	local currback=""
+	local lastback=""
+	local opt="-aHX --delete"
+	local optrsh="--rsh=ssh"
+	local allowSrcRemote=1
+	local allowDstRemote=0
 
-if [ $# -lt 2 ]; then
-    printf "Error: wrong number or arguments (%d given, >=2 expected)", "$#"
-    exit -1
-fi
+	if [ $# -lt 2 ]; then
+		printf "Error: wrong number or arguments (%d given, >=2 expected)", "$#"
+		exit -1
+	fi
 
-if [ "$1" == "--hist" ] ; then
-    hist=1
-    shift
-elif [ "$1" == "--inet" ] ; then
-    opt="-rLt --size-only --partial"
-    shift
-elif [ "${1%%=*}" == "--inetlog" ] ; then
-    inetlog=1
-    logfile=${1#*=}
-    opt="-rLt --size-only --partial"
-#    optlog3="-n --info=DEL -r --delete --existing --ignore-existing"    
-    optlog3="-n --info=DEL,STATS0 --debug=NONE -r --delete --force --delete-excluded --existing --ignore-existing"
-    shift
-elif [ "$1" == "--ssh" ] ; then
-    allowDstRemote=1
-    allowSrcRemote=0
-    shift
-    sshusersrv="${2%%:*}"
-    opt="-aH --partial"
-    opt="$opt --password-file=/root/ssl.private/$sshusersrv.password"
-    optrsh="--rsh=ssh -i ~/ssl.private/$sshusersrv"
-elif [ "${1%%=*}" == "--sshlog" ] ; then
-    # NB: euserv.deßs remote RSync does currently not support --info=DEL,STATS0
-    # so this will not work.
-    inetlog=1	
-    logfile=${1#*=}
-    allowDstRemote=1
-    allowSrcRemote=0
-    shift
-    sshusersrv="${2%%:*}"
-    opt="-aH --partial"
-    opt="$opt --password-file=/root/ssl.private/$sshusersrv.password"
-    optrsh="--rsh=ssh -i ~/ssl.private/$sshusersrv"
-    optlog3="-n --info=DEL,STATS0 --debug=NONE -r --delete --force --delete-excluded --existing --ignore-existing"
-    optlog3="$optlog3 --password-file=/root/ssl.private/$sshusersrv.password"
-fi
+	if [ "$1" == "--hist" ] ; then
+		hist=1
+		shift
+	elif [ "$1" == "--inet" ] ; then
+		opt="-rLt --size-only --partial"
+		shift
+	elif [ "${1%%=*}" == "--inetlog" ] ; then
+		inetlog=1
+		logfile=${1#*=}
+		opt="-rLt --size-only --partial"
+		# optlog3="-n --info=DEL -r --delete --existing --ignore-existing"
+		optlog3="-n --info=DEL,STATS0 --debug=NONE -r --delete --force --delete-excluded --existing --ignore-existing"
+		shift
+	elif [ "$1" == "--ssh" ] ; then
+		allowDstRemote=1
+		allowSrcRemote=0
+		shift
+		sshusersrv="${2%%:*}"
+		opt="-aH --partial"
+		opt="$opt --password-file=/root/ssl.private/$sshusersrv.password"
+		optrsh="--rsh=ssh -i ~/ssl.private/$sshusersrv"
+	elif [ "${1%%=*}" == "--sshlog" ] ; then
+		# NB: euserv.de's remote RSync does currently not support --info=DEL,STATS0
+		# so this will not work.
+		inetlog=1	
+		logfile=${1#*=}
+		allowDstRemote=1
+		allowSrcRemote=0
+		shift
+		sshusersrv="${2%%:*}"
+		opt="-aH --partial"
+		opt="$opt --password-file=/root/ssl.private/$sshusersrv.password"
+		optrsh="--rsh=ssh -i ~/ssl.private/$sshusersrv"
+		optlog3="-n --info=DEL,STATS0 --debug=NONE -r --delete --force --delete-excluded --existing --ignore-existing"
+		optlog3="$optlog3 --password-file=/root/ssl.private/$sshusersrv.password"
+	fi
 
-#### NB: %/ removes a trailing slash if it exists
-local src="${1%/}"
-local dst="${2%/}"
-shift 2
+	#### NB: %/ removes a trailing slash if it exists
+	local src="${1%/}"
+	local dst="${2%/}"
+	shift 2
 
-if [ $# -gt 0 ]; then
-    # we got additional rsync parameters
-    prm="$*"
-else
-    prm=""
-fi
+	if [ $# -gt 0 ]; then
+		# we got additional rsync parameters
+		prm="$*"
+	else
+		prm=""
+	fi
 
-if [[ "$src" == *":"* ]] ; then
-    if [ $allowSrcRemote -eq 1 ]; then
-        if [ $debug -eq 1 ]; then 
-            printf "Source Directory not checked, its on another computer.\n"
-        fi
-    else
-	printf "Error: Source Directory cannot be on another computer.\n"
-	exit -2
-    fi
-elif [ ! -d "$src" ]; then
-    printf "Error: Source Directory %s does not exist\n" "$src"
-    exit -3
-else
-    if [ $debug -eq 1 ]; then 
-        printf "Source Directory OK.\n"
-    fi
-fi
-
-
-if [[ "$dst" == *":"* ]] ; then
-    if [ $allowDstRemote -eq 1 ]; then
-        if [ $debug -eq 1 ]; then 
-            printf "Dest Directory not checked, its on another computer.\n"
-        fi
-    else
-        printf "Error: Dest Directory cannot be on another computer.\n"
-        exit -4
-    fi
-elif [ ! -d "$dst" ]; then
-    # Not an error, Dest-Dir will be created by rsync
-    if [ $debug -eq 1 ]; then 
-        printf "Dest Directory will be created.\n"
-    fi
-else
-    if [ $debug -eq 1 ]; then 
-        printf "Dest Directory OK.\n"
-    fi
-fi
-
-if [ ! -z $logfile ] && [ "${logfile:0:1}" != "/" ] ; then
-    printf "Error: Logfilename %s does not start with a slash.\n" "$logfile"
-    exit -6
-fi
-
-if [ $hist -eq 1 ] ; then
-    ##### HISTORY MODE #####
-    currback="$dst/$(date +%Y%m%d)"
-    lastback=$(ls -rd $dst/2* 2>/dev/null | head -1)
-    if [ "$currback" == "$lastback" ] ; then
-        # target directory already exists
-        # this means today a backup was already executed
-        lastback=$(ls -rd $dst/2* 2>/dev/null | head -2 | tail -1)
-        if [ "$currback" == "$lastback" ] ; then
-            # No other directory found
-            # So we have only one directory, thus use Initial mode!
-            lastback=""
-        fi
-    fi
-
-    if [ "$lastback" == "" ]; then
-        histparm=""
-        printf "Backing up %s in history mode to %s (Initial Backup)\n" \
-            "$src" "$currback"
-    else
-        histparm="--link-dest=$lastback"
-        printf "Backing up %s in history mode to %s (based on %s)\n" \
-            "$src" "$currback" "$lastback"
-    fi
-else
-    currback="$dst"
-    lastback=""
-    printf "Backing up %s to %s\n" "$src" "$currback"
-fi
+	if [[ "$src" == *":"* ]] ; then
+		if [ $allowSrcRemote -eq 1 ]; then
+			if [ $debug -eq 1 ]; then
+				printf "Source Directory not checked, its on another computer.\n"
+			fi
+		else
+			printf "Error: Source Directory cannot be on another computer.\n"
+			exit -2
+		fi
+	elif [ ! -d "$src" ]; then
+		printf "Error: Source Directory %s does not exist\n" "$src"
+		exit -3
+	else
+		if [ $debug -eq 1 ]; then
+			printf "Source Directory OK.\n"
+		fi
+	fi
 
 
-if [[ "$dst" == *":"* ]] ; then
-    if [ $debug -eq 1 ]; then 
-        printf "Dest Directory not created, its on another computer.\n"
-    fi
-elif [ ! -d "$currback" ]; then
-    mkdir -p "$currback"
-    if [ $? -eq 0 ] ; then
-        if [ $debug -eq 1 ]; then 
-            printf "Created Dest Directory %s\n" "$curback"
-        fi
-    else
-        printf "Error creating Dest Directory %s\n" "$currback"
-        exit -7
-    fi
-fi
+	if [[ "$dst" == *":"* ]] ; then
+		if [ $allowDstRemote -eq 1 ]; then
+			if [ $debug -eq 1 ]; then
+				printf "Dest Directory not checked, its on another computer.\n"
+			fi
+		else
+			printf "Error: Dest Directory cannot be on another computer.\n"
+			exit -4
+		fi
+	elif [ ! -d "$dst" ]; then
+		# Not an error, Dest-Dir will be created by rsync
+		if [ $debug -eq 1 ]; then
+			printf "Dest Directory will be created.\n"
+		fi
+	else
+		if [ $debug -eq 1 ]; then
+			printf "Dest Directory OK.\n"
+		fi
+	fi
 
-if [ $debug -eq 1 ]; then
-   opt="$opt --progress"
-fi   
+	if [ ! -z $logfile ] && [ "${logfile:0:1}" != "/" ] ; then
+		printf "Error: Logfilename %s does not start with a slash.\n" "$logfile"
+		exit -6
+	fi
 
-# NB: source directory is given with a trailing slash.
-# This way rsync will not create a directory of same name as source
-# in the destination folder but sync all files inside source directory
-# into the target directory
-local start_date="$(date -u +"%s")"
-if [ $debug -eq 1 ]; then 
-  printf "Executing rsync %s %s %s %s %s/ %s\n" "$opt" "$optrsh" "$histparm" "$prm" "$src" "$currback"
-fi
-rsync $opt "$optrsh" $histparm $prm $src/ $currback
-local rsync_rc="$?"
+	if [ $hist -eq 1 ] ; then
+		##### HISTORY MODE #####
+		currback="$dst/$(date +%Y%m%d)"
+		lastback=$(ls -rd $dst/2* 2>/dev/null | head -1)
+		if [ "$currback" == "$lastback" ] ; then
+			# target directory already exists
+			# this means today a backup was already executed
+			lastback=$(ls -rd $dst/2* 2>/dev/null | head -2 | tail -1)
+			if [ "$currback" == "$lastback" ] ; then
+				# No other directory found
+				# So we have only one directory, thus use Initial mode!
+				lastback=""
+			fi
+		fi
 
-local diff_date=$(( $(date -u +"%s") - $start_date ))
-local elapsed=$(date +%H:%M:%S -u -d @"$diff_date")
-if [ $rsync_rc -eq 0 ] ; then
-    printf "Backup of %s completed (%s).\n" "$src" "$elapsed"
-else
-    printf "Backup of %s ended in error. RSync RC=%d (%s).\n" \
-        "$src" "$rsync_rc" "$elapsed"
-fi
+		if [ "$lastback" == "" ]; then
+			histparm=""
+			printf "Backing up %s in history mode to %s (Initial Backup)\n" \
+				"$src" "$currback"
+		else
+			histparm="--link-dest=$lastback"
+			printf "Backing up %s in history mode to %s (based on %s)\n" \
+				"$src" "$currback" "$lastback"
+		fi
+	else
+		currback="$dst"
+		lastback=""
+		printf "Backing up %s to %s\n" "$src" "$currback"
+	fi
 
-# If we want a log of to be deleted files, we do it this way:
-# execute rsync in dry mode to list the files
-if [ $inetlog -eq 1 ] ; then
-    printf "Writing deleted filelist of %s to %s\n" "$src" "$logfile"
 
-    # create directory for LogFile if it does not exist
-    if [ ! -d $(dirname "$logfile") ]; then
-        mkdir -p "$(dirname "$logfile")"
-    fi
+	if [[ "$dst" == *":"* ]] ; then
+		if [ $debug -eq 1 ]; then
+			printf "Dest Directory not created, its on another computer.\n"
+		fi
+	elif [ ! -d "$currback" ]; then
+		mkdir -p "$currback"
+		if [ $? -eq 0 ] ; then
+			if [ $debug -eq 1 ]; then
+				printf "Created Dest Directory %s\n" "$curback"
+		fi
+	else
+		printf "Error creating Dest Directory %s\n" "$currback"
+		exit -7
+		fi
+	fi
 
-    if [ $debug -eq 1 ]; then 
-        printf "Executing rsync %s %s %s %s/ %s\n" \
-            "$optrsh" "$prm" "$optlog3" "$src" "$currback"
-    fi
-    rsync "$optrsh" $prm $optlog3 $src/ $currback |\
-           sed -e 's/^deleting //' -e '/sending incremental file list/d' >$logfile
-fi
+	if [ $debug -eq 1 ]; then
+		opt="$opt --progress"
+	fi
+
+	# NB: source directory is given with a trailing slash.
+	# This way rsync will not create a directory of same name as source
+	# in the destination folder but sync all files inside source directory
+	# into the target directory
+	local start_date="$(date -u +"%s")"
+	if [ $debug -eq 1 ]; then 
+		printf "Executing rsync %s %s %s %s %s/ %s\n" "$opt" "$optrsh" "$histparm" "$prm" "$src" "$currback"
+	fi
+	rsync $opt "$optrsh" $histparm $prm $src/ $currback
+	local rsync_rc="$?"
+
+	local diff_date=$(( $(date -u +"%s") - $start_date ))
+	local elapsed=$(date +%H:%M:%S -u -d @"$diff_date")
+	if [ $rsync_rc -eq 0 ] ; then
+		printf "Backup of %s completed (%s).\n" "$src" "$elapsed"
+	else
+		printf "Backup of %s ended in error. RSync RC=%d (%s).\n" \
+			"$src" "$rsync_rc" "$elapsed"
+	fi
+
+	# If we want a log of to be deleted files, we do it this way:
+	# execute rsync in dry mode to list the files
+	if [ $inetlog -eq 1 ] ; then
+		printf "Writing deleted filelist of %s to %s\n" "$src" "$logfile"
+
+		# create directory for LogFile if it does not exist
+		if [ ! -d $(dirname "$logfile") ]; then
+			mkdir -p "$(dirname "$logfile")"
+		fi
+
+		if [ $debug -eq 1 ]; then
+			printf "Executing rsync %s %s %s %s/ %s\n" \
+				"$optrsh" "$prm" "$optlog3" "$src" "$currback"
+		fi
+		rsync "$optrsh" $prm $optlog3 $src/ $currback |\
+			sed -e 's/^deleting //' -e '/sending incremental file list/d' >$logfile
+	fi
 
 }
 
-function backup_rsync_print
-{
-# check: Parameter must be 3!
-if [ $# -ne 3 ]; then
-    printf "Error: wrong number or arguments (%d given, =3 expected)", "$#"
-    exit -1
-fi
+function backup_rsync_print {
+	# check: Parameter must be 3!
+	if [ $# -ne 3 ]; then
+		printf "Error: wrong number or arguments (%d given, =3 expected)", "$#"
+		exit -1
+	fi
 
-local src="${2%/}"
-local dst="${3%/}"
-local log="$1"
+	local src="${2%/}"
+	local dst="${3%/}"
+	local log="$1"
 
-if [ ! -d "$dst" ] ; then
-    printf "Error: Dest Directory %s does not exist or no directory.\n" \
-        "$dst"
-    exit -2
-fi
+	if [ ! -d "$dst" ] ; then
+		printf "Error: Dest Directory %s does not exist or no directory.\n" \
+			"$dst"
+		exit -2
+	fi
 
-if [ ! -f "$log" ] ; then
-    printf "Error: Logfile %s does not exist.\n" "$log"
-    exit -3
-fi
+	if [ ! -f "$log" ] ; then
+		printf "Error: Logfile %s does not exist.\n" "$log"
+		exit -3
+	fi
 
-if [ -s "$log" ]; then
-    printf "%s has deleted files. you may want to use\n" "$src"
-    printf "xargs -d \x5c\x5cn -a %s -I _ rm -d \"%s/_\"\n" "$log" "$dst"
-    printf "to delete the files on the internet storage. Filelist: \n"
-    cat $log
-fi
+	if [ -s "$log" ]; then
+		printf "%s has deleted files. you may want to use\n" "$src"
+		printf "xargs -d \x5c\x5cn -a %s -I _ rm -d \"%s/_\"\n" "$log" "$dst"
+		printf "to delete the files on the internet storage. Filelist: \n"
+		cat $log
+	fi
 }
 
 
